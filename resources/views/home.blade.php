c9d473bcc38e10715be01348209189ad3652f7cc
Hariharan Thiagarajan <hariharansmm@gmail.com>
2021-02-24 00:09:50 +1300

    Fifth Commit
    Added SVG
    Created rough Home Page
    Added Fonts from GFonts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fhlz1-1.fna.fbcdn.net/v/t51.2885-19/s320x320/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fhlz1-1.fna.fbcdn.net&_nc_ohc=wbBX-0-mRFwAX9hkVeE&tp=1&oh=f1bf5c7848c1e4daf82e87dcd0ade088&oe=605CA21F" class="rounded-circle" height="200px" width="200px">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Name</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>Num</strong> posts</div>
+                <div class="pr-5"><strong>Num</strong> followers</div>
+                <div class="pr-5"><strong>Num</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">Heading</div>
+            <div>Description</div>
+            <div><a href="#">URL</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fhlz1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c1.0.825.825a/s640x640/151387931_248131726986063_7146800644906673262_n.jpg?_nc_ht=instagram.fhlz1-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=elv8IhAKlEAAX_Z41oq&tp=1&oh=2a82144f551d3413da45827310aa23b4&oe=605EDCF2" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fhlz1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.20.866.866a/s640x640/150607721_1232644437130448_5432726904860811056_n.jpg?_nc_ht=instagram.fhlz1-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=LWkl5luZdusAX_wZIsf&tp=1&oh=a31f6e1dc2e3054cb5aa59eee439d205&oe=60602578" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fhlz1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.65.943.943a/s640x640/148526975_459153691922829_5035445392948180720_n.jpg?_nc_ht=instagram.fhlz1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=JwA3UY_b24wAX-z34-m&tp=1&oh=87d087d97eb79828a46cd900cf50e8e7&oe=605F2F02" class="w-100">
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/instaCloneLogo.svg" alt="Logo" style="height: 20px; border-right: 1px solid #333333" class="pr-2"></div>
+                    <div class="pl-2">InstaClone</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -45,7 +46,7 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-                            
+
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
